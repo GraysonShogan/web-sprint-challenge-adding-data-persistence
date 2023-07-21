78e2fa23ed3cb147182129ec2d6b9dbb9d358b59
Grayson Shogan <graysontoddshogan@gmail.com>
2023-07-21 11:21:19 -0700

    done

--- api/resource/model.js
+++ api/resource/model.js
@@ -1,1 +1,22 @@
-// build your `Resource` model here
+const db = require("../../data/dbConfig");
+
+async function find() {
+  return db("resources");
+}
+
+function findById(resource_id) {
+  return db("resources").where({ resource_id });
+}
+
+async function create(resource) {
+  return db("resources")
+    .insert(resource)
+    .then((resource_id) => {
+      return findById(resource_id).first();
+    });
+}
+
+module.exports = {
+  find,
+  create,
+};
