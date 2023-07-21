609cd64113a471e33d053a9d2e24efa4b361fba8
Grayson Shogan <graysontoddshogan@gmail.com>
2023-07-21 11:21:24 -0700

    done

--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,32 @@
-// build your `/api/projects` router here
+const router = require("express").Router();
+
+const Projects = require("../project/model");
+
+router.get("/", (req, res, next) => {
+  Projects.find()
+    .then((projects) => {
+      res.status(200).json(isFalse(projects));
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.post("/", (req, res, next) => {
+  Projects.create(req.body)
+    .then((project) => {
+      res.status(201).json(isFalse(project)[0]);
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+
+const isFalse = (projects) => {
+  return projects.map((project) => ({
+    ...project,
+    project_completed: project.project_completed ? true : false,
+  }));
+};
+
+module.exports = router;
--- api/task/router.js
+++ api/task/router.js
@@ -1,1 +1,33 @@
-// build your `/api/tasks` router here
+const router = require("express").Router();
+const Tasks = require("./model");
+
+router.get("/", (req, res, next) => {
+  Tasks.find().then((tasks) => {
+    res.json(isFalse(tasks));
+  });
+});
+
+router.post("/", (req, res, next) => {
+  Tasks.create(req.body)
+    .then((task) => {
+      res.json(isFalse(task)[0]);
+    })
+    .catch(next);
+});
+
+router.use((err, req, res, next) => {
+  res.status(500).json({
+    customMessage: "something went wrong inside the resource router",
+    message: err.message,
+    stack: err.stack,
+  });
+});
+
+const isFalse = (tasks) => {
+  return tasks.map((proj) => ({
+    ...proj,
+    task_completed: proj.task_completed ? true : false,
+  }));
+};
+
+module.exports = router;
